0a8215bdf7ca512ab22d607410a07ed2018dae44
Rico042002 <rico042002@gmail.com>
2022-01-13 22:24:47 -0600

    Add files via upload

---
+++ bmi.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  double height, weight, bmi;
+  
+  // Ask user for their height
+  
+  std::cout << "Type in your height (m): ";
+  std::cin >> height;
+  
+  // Now ask the user for their weight and calculate BMI
+  
+  std::cout << "Type in your weight (kg): ";
+  std::cin >> weight;
+
+  bmi = weight / (height * height);
+  
+  std::cout << "Your BMI is " << round(bmi) << "\n";
+  
+  
+  
+  
+  
+  return 0;
+
+}
